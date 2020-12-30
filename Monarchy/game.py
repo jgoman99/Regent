11ac3e2ee29232af100ff33f2c7d81641a09fe17
John Friedman <jgfriedman99@gmail.com>
2020-12-30 13:58:37 -0800

    added thiefs

--- Monarchy/game.py
+++ Monarchy/game.py
@@ -14,13 +14,13 @@ class Person:
     def __init__(self):
         name = "Ted"
         culture = rd.randint(0,2)
-        job = "Farmer"
+        job = "farmer"
         health = .75        
         money = 0
         
         
         self.attributes = {"name": name, "culture": culture, "job": job, "jobwage": None,
-                           "health": health}
+                           "health": health, "content" : 75}
         self.resources = {"money": money}
         self.instances = {"leisure_hours": None, "labor_hours": None }
         self.taxes = {"flat": 0, "prop": 0}
@@ -30,17 +30,36 @@ class Person:
         self.updateJobWage()
         
     def updateJobWage(self):
-        if self.attributes["job"] == "Farmer":
+        job = self.attributes["job"]
+        if job == "farmer":
             self.attributes["jobwage"] = 1
+        elif job == "thief":
+            self.attributes["jobwage"] = 0
         else:
             self.attributes["jobwage"] = None
             
         
-    def workJob(self):
-        self.allocateTime()
-        labor = self.instances["labor_hours"]
+    def workJob(self,person):
+        job = self.attributes["job"]
+        if job == "farmer":
+            self.allocateTime()
+            labor = self.instances["labor_hours"]
+            self.resources["money"] += self.attributes["jobwage"] * labor
+        elif job == "thief":
+            self.steal(person)
+            
+    def steal(self, people):
+        health = self.attributes["health"]
+        steal_chance = round((rd.random()+.49)*np.sqrt(health))
         
-        self.resources["money"] += self.attributes["jobwage"] * labor
+        if steal_chance == 1:
+            amount = 4
+            for person in people:
+                if (amount > person.resources["money"]):
+                    amount = person.resources["money"]
+                
+                person.resources["money"] -= amount
+                self.resources["money"] += amount
         
 
         
@@ -72,19 +91,47 @@ class Person:
         self.instances["leisure_hours"], self.instances["labor_hours"] = leisure_hours, labor_hours
     
     def consumeGoods(self):
+        #may need to tweak content formula
         money = self.resources["money"]
+        job = self.attributes["job"]
         
-        if money > 4:
-            self.attributes["health"] += .2
-            self.resources["money"] -= 4
-        else:
-            self.attributes["health"] += (money-2)/10
-            self.resources["money"] = 0
+        if job == "farmer":
+            if money > 4:
+                self.attributes["health"] += .2
+                self.resources["money"] -= 4
+                self.attributes["content"] += 2
+            else:
+                self.attributes["health"] += (money-2)/10
+                self.resources["money"] = 0
+                self.attributes["content"] += (money-2)*10
+                
+            if self.attributes["health"] > 1:
+                self.attributes["health"] = 1
             
-        if self.attributes["health"] > 1:
-            self.resources["health"] = 1
+            if self.attributes["content"] > 100:
+                self.attributes["content"] = 100
+        elif job == "thief":
+            if money > 4:
+                self.attributes["health"] += .2
+                self.resources["money"] -= 4
+            else:
+                self.attributes["health"] += (money-2)/10
+                self.resources["money"] = 0
+         
+            if self.attributes["health"] > 1:
+                self.attributes["health"] = 1
+            
         
         
+    
+    def updateJob(self):
+        content, health = self.attributes["content"], self.attributes["health"]
+        if content < 20 and health >= 0.5:
+            self.attributes["job"] = "thief"
+            
+        self.updateJobWage()
+            
+            
             
         
 class Tax:
@@ -128,6 +175,7 @@ class Tax:
         
 class Game:
     def __init__(self):
+        #currently up to 1000 people with all calculations done in less than a second
         self.people = [Person() for i in range(0,100)]
         self.taxes = []
         self.treasury = {"money": 0 }
@@ -135,7 +183,15 @@ class Game:
 
     def work(self):
         for person in self.people:
-            person.workJob()
+            job = person.attributes["job"]
+            victims = []
+            # Yes you can steal from yourself. Meh. fine for sim reasons
+            if job == "thief":
+                victim1 = self.people[rd.randint(0,len(self.people)-1)]
+                victim2 = self.people[rd.randint(0,len(self.people)-1)]
+                victims.append(victim1)
+                victims.append(victim2)
+            person.workJob(victims)
             
     def taxPeople(self):
         for person in self.people:
@@ -146,11 +202,15 @@ class Game:
             if (person.resources["money"] < tax):
                 self.treasury["money"] += person.resources["money"]
                 person.resources["money"] = 0
+                person.attributes["content"] -= 40
             else:
                 self.treasury["money"] += tax
                 person.resources["money"] -= tax
+            
                 
-                
+    def updateJobs(self):
+        for person in self.people:
+            person.updateJob()
             
             
             
@@ -175,16 +235,42 @@ class Game:
         for person in self.people:
             person.consumeGoods()
             
+    def returnPeopleStats(self,key,aggregate_operation):
+        vec = []
+        for person in self.people:
+            attrs = person.attributes
+            resources = person.resources
+            taxes = person.taxes
+            instances = person.instances
+            
+            combined_dict = {**attrs,**resources,**taxes,**instances}
+            
+            to_append = combined_dict[key]
+            vec.append(to_append)
+            
+        if aggregate_operation == "none":
+            pass
+        elif aggregate_operation == 'mean':
+            vec = np.mean(vec)
+        elif aggregate_operation == 'sum':
+            vec = np.sum(vec)
+        elif aggregate_operation == "unique":
+            vec = np.unique(vec)
+        else:
+            print("error: aggregate_operation not recognized")
+        return vec
+    
     def nextTick(self):
         self.updatePeoplesTaxes()
         self.work()
         self.taxPeople()
         self.consume()
+        self.updateJobs()
         
 
-# g1 = Game()
-# t1 = Tax(["culture","name"],[[0,2],"Ted"],["=","="],"xtax","money","flat",2)
-# t2 = Tax(["culture","name"],[[0,2],"Ted"],["=","="],"xtax","money","prop",.1)
-# g1.taxes.append(t1)
-# g1.taxes.append(t2)
-# g1.nextTick()
+g1 = Game()
+t1 = Tax(["culture","name"],[[0,2],"Ted"],["=","="],"xtax","money","flat",2)
+t2 = Tax(["culture","name"],[[0,2],"Ted"],["=","="],"xtax","money","prop",.1)
+g1.taxes.append(t1)
+g1.taxes.append(t2)
+g1.nextTick()
